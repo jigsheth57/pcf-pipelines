3d446a335079611d343d22dbd706307ad7839459
Jignesh Sheth <jsheth@pivotal.io>
2017-09-16 15:10:42 -0500

    update request timeout
    Updated timeout to upload tile to ops manager

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -70,4 +70,4 @@ if [ -n "$STEMCELL_VERSION" ]; then
 fi
 
 FILE_PATH=`find ./pivnet-product -name *.pivotal`
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p $FILE_PATH
+om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 7200 upload-product -p $FILE_PATH
