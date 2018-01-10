88c1c0d4bbe9b03dd84902aba73569cb8b136736
Jignesh Sheth <jsheth@pivotal.io>
2018-01-10 07:09:14 -0600

    error fix
    pcf-pipelines/tasks/upload-product-and-stemcell/task.sh: line 83:
    syntax error near unexpected token `<<<'

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -80,7 +80,7 @@ fi
 
 # Should the slug contain more than one product, pick only the first.
 FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
-<<<<<<< HEAD
+<<< HEAD
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 7200 upload-product -p $FILE_PATH
 =======
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
@@ -92,4 +92,4 @@ om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --request-timeout 3600 \
   upload-product \
   -p $FILE_PATH
->>>>>>> pivotal-cf/master
+>>> pivotal-cf/master
