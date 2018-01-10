26a0f91a160097ed2de0d28c9b08dd02d80ff9c5
Jignesh Sheth <jsheth@pivotal.io>
2018-01-10 07:13:13 -0600

    update

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -80,9 +80,6 @@ fi
 
 # Should the slug contain more than one product, pick only the first.
 FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
-<<< HEAD
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 7200 upload-product -p $FILE_PATH
-=======
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --client-id "${OPSMAN_CLIENT_ID}" \
   --client-secret "${OPSMAN_CLIENT_SECRET}" \
@@ -92,4 +89,3 @@ om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --request-timeout 3600 \
   upload-product \
   -p $FILE_PATH
->>> pivotal-cf/master
